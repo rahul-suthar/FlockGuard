a420ac0214d191e82dd6b0b1bbe4366c55e5920f
Rahul Suthar <154016388+Rahul-Suthar@users.noreply.github.com>
2025-09-04 22:33:16 +0530

    frontend Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex p-5 h-screen items-start justify-center bg-blue-200'>
+        <h1><span className='text-4xl font-bold text-black'>BioFarm</span><span className='text-3xl'> - safer farms, stroger livelihoods</span></h1>
+      </div>
+    </>
+  )
+}
+
+export default App
