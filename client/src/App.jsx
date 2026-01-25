e7422fc0dee5632c6c5af20b075ca22d8c566bdf
rahul-suthar <154016388+rahul-suthar@users.noreply.github.com>
2026-01-25 11:12:31 +0000

    chore: optimize with lazy loading, and clean deps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import Onboarding from "./pages/Onboarding";
 import NavBar from "./components/NavBar.jsx";
-import Contact from "./pages/Contact.jsx";
-import About from "./pages/About.jsx";
 import Footer from "./components/Footer.jsx";
-import Terms from "./pages/Terms.jsx";
-import Privacy from "./pages/Privacy.jsx";
 import { AnimatePresence } from "framer-motion";
 import AnimatedPage from "./components/AnimatedPage.jsx";
-import NotFound from "./pages/NotFound.jsx";
+
+const Onboarding = lazy(() => import("./pages/Onboarding"));
+const Contact = lazy(() => import("./pages/Contact.jsx"));
+const About = lazy(() => import("./pages/About.jsx"));
+const Terms = lazy(() => import("./pages/Terms.jsx"));
+const Privacy = lazy(() => import("./pages/Privacy.jsx"));
+const NotFound = lazy(() => import("./pages/NotFound.jsx"));
 
 function App() {
   const location = useLocation();
@@ -17,56 +19,64 @@ function App() {
       <NavBar />
       <main className="flex-1">
         <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route
-              path="/"
-              element={
-                <AnimatedPage>
-                  <Onboarding />
-                </AnimatedPage>
-              }
-            />
-            <Route
-              path="/about"
-              element={
-                <AnimatedPage>
-                  <About />
-                </AnimatedPage>
-              }
-            />
-            <Route
-              path="/contact-us"
-              element={
-                <AnimatedPage>
-                  <Contact />
-                </AnimatedPage>
-              }
-            />
-            <Route
-              path="/privacy-policy"
-              element={
-                <AnimatedPage>
-                  <Privacy />
-                </AnimatedPage>
-              }
-            />
-            <Route
-              path="/terms-of-service"
-              element={
-                <AnimatedPage>
-                  <Terms />
-                </AnimatedPage>
-              }
-            />
-            <Route
-              path="*"
-              element={
-                <AnimatedPage>
-                  <NotFound />
-                </AnimatedPage>
-              }
-            />
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="flex h-[70dvh] items-center justify-center text-red-500 font-bold">
+                Loading...
+              </div>
+            }
+          >
+            <Routes location={location} key={location.pathname}>
+              <Route
+                path="/"
+                element={
+                  <AnimatedPage>
+                    <Onboarding />
+                  </AnimatedPage>
+                }
+              />
+              <Route
+                path="/about"
+                element={
+                  <AnimatedPage>
+                    <About />
+                  </AnimatedPage>
+                }
+              />
+              <Route
+                path="/contact-us"
+                element={
+                  <AnimatedPage>
+                    <Contact />
+                  </AnimatedPage>
+                }
+              />
+              <Route
+                path="/privacy-policy"
+                element={
+                  <AnimatedPage>
+                    <Privacy />
+                  </AnimatedPage>
+                }
+              />
+              <Route
+                path="/terms-of-service"
+                element={
+                  <AnimatedPage>
+                    <Terms />
+                  </AnimatedPage>
+                }
+              />
+              <Route
+                path="*"
+                element={
+                  <AnimatedPage>
+                    <NotFound />
+                  </AnimatedPage>
+                }
+              />
+            </Routes>
+          </Suspense>
         </AnimatePresence>
       </main>
       <Footer />
