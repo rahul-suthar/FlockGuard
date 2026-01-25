ac8c72dc7f54c8a3e30cbbe47b62067bc82e5ff3
rahul-suthar <154016388+rahul-suthar@users.noreply.github.com>
2026-01-25 10:56:41 +0000

    feat: implemented automatic sitemap generation

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    allowedHosts: "all",
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://flock-guard.vercel.app/",
+      dynamicRoutes: [
+        "/",
+        "/about",
+        "/contact-us",
+        "/privacy-policy",
+        "/terms-of-service",
+      ],
+    }),
+  ]
 });
